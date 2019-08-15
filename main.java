4258e1b20f841d1e67baf1e744d91b3d0c522cfd
travisduggins <travisduggins47@gmail.com>
2019-08-15 10:56:30 -0700

    this is the point in time when travis deleted feature1's stuff
    Signed-off-by: travisduggins <travisduggins47@gmail.com>

--- main.java
+++ main.java
@@ -1,7 +1,6 @@
 class myClass {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("I Love This Planet!");
         System.out.println("This Planet Is Wonderful!");
         System.out.println("This Is Some More Text!");
     }
