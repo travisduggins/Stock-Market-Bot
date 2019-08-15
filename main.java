e7e3515ed869f21f4df1cf659c7b0cacccfaff1e
travisduggins <travisduggins47@gmail.com>
2019-08-15 10:47:50 -0700

    This is the point in time when i traivs have COPLETELY finished Feature2
    Signed-off-by: travisduggins <travisduggins47@gmail.com>

--- main.java
+++ main.java
@@ -1,7 +1,7 @@
 class myClass {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        
         System.out.println("This Planet Is Wonderful!");
+        System.out.println("This Is Some More Text!");
     }
 }
